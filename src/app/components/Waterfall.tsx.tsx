6becbf88a7f16be6603445c6e53c7a608a7d44a9
kricn <mnyt2436@126.com>
2023-10-28 01:28:00 +0800

    完善瀑布流组件并添加注释

--- src/app/components/Waterfall.tsx.tsx
+++ src/app/components/Waterfall.tsx.tsx
@@ -21,44 +21,68 @@ interface WaterfallProps {
  colGap?: number
 }
 
+/** 底部预留高度，表示距离底部还剩多少距离时开始显示图片 */
+const remainHeight = 50
 
+/**
+ * 瀑布流组件
+ * 
+ */
 const Waterfall = (props: WaterfallProps) => {
 
   const { items, itemWidth, columns, rowGap = 10, colGap = 10 } = props;
+  /** 图片信息数据 */
+  const [images, setImages] = useState<WaterfallItem[]>([...items]);
   const initialized = useRef(false)
 
+  /** 记录所有图片节点 */
+  let imagesNodes: HTMLImageElement[] = []
+
+  /** 记录瀑布流所有列的高度 */
   const columnsHeight = new Array(columns).fill(0);
+  /** 记录当前哪一列的高度最小 */
   let minCol = 0;
 
-  function isContain(dom: HTMLElement) {
+  /** 判断一第图片是否在可视区内 */
+  function isContain(dom: HTMLImageElement) {
     const totalHeight = window.innerHeight || document.documentElement.clientHeight;
     const totalWidth = window.innerWidth || document.documentElement.clientWidth;
     // 当滚动条滚动时，top, left, bottom, right时刻会发生改变。
-    const { top, right, bottom, left } = dom.getBoundingClientRect();
-    console.log(dom)
-    console.log(dom.getBoundingClientRect())
-    return (top >= 0 && left >= 0 && right <= totalWidth && bottom <= totalHeight);
+    const { top, right, left } = dom.getBoundingClientRect();
+    return (top >= 0 && left >= 0 && right <= totalWidth && top <= totalHeight + remainHeight);
   }
 
+  /** 
+   * 页面滚动监听回调函数
+   * 更新图片节点样式
+   */
   function handleScroll() {
-    const imgs = document.querySelectorAll('img');
-    for (let i = 0; i < imgs.length; i++) {
-      console.log(items[i])
-      if (!items[i] || !items[i]?.hide) break ;
-      const img = imgs[i]
-      items[i].hide = !isContain(img);
+    for (let i = 0; i < imagesNodes.length; i++) {
+      if (!images[i]) break ;
+      const img = imagesNodes[i]
+      if (images[i]?.hide) {
+        images[i].hide = !isContain(img);
+        setImages([...images])
+      }
     }
   }
   
+  /** 组件初始化 */
   useEffect(() => {
-    if (initialized.current) return ;
+    if (initialized.current) {
+      return () => {
+        document.removeEventListener('scroll', handleScroll)
+      }
+    };
     initialized.current = true;
-    const imgs = document.querySelectorAll('img');
-    for (let i = 0; i < imgs.length; i++) {
-      if (!items[i]) break ;
-      const img = imgs[i]
-      items[i].height = img.height
-      items[i].width = img.width
+    /** 记录所有节点 */
+    imagesNodes = document.querySelectorAll('img') as any;
+    /** 计算每张图片的位置 */
+    for (let i = 0; i < imagesNodes.length; i++) {
+      if (!images[i]) break ;
+      const img = imagesNodes[i]
+      images[i].height = img.height
+      images[i].width = img.width
       img.style.left = (minCol * (itemWidth + colGap)) + 'px'
       const minHeight = columnsHeight[minCol]
       img.style.top = minHeight + rowGap + 'px'
@@ -66,18 +90,19 @@ const Waterfall = (props: WaterfallProps) => {
       columnsHeight[minCol] += img.height + rowGap
       // 更新最小列
       minCol = columnsHeight.indexOf(Math.min(...columnsHeight))
+      /** 更新每一张图片信息 */
+      setImages([...images])
     }
+    /** 初始化时监听页面滚动 */
     document.addEventListener('scroll', handleScroll)
+    /** 刚开始时，自动调用一次滚动函数 */
     handleScroll()
-    return () => {
-      document.removeEventListener('scroll', handleScroll)
-    }
   }, [])
 
   return (
     <div className={styles.container}>
       {
-        items.map((img, index) => (
+        images.map((img, index) => (
           <img 
             key={index}
             src={img.src} 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { readdirSync } from 'fs'
-import styles from './page.module.css'
 import path from 'path'
 import Waterfall from './components/Waterfall.tsx'
 
 
-// 写一个瀑布流代码
 export default function Home() {
   /** 读取images文件 */
   const images = readdirSync(path.join(process.cwd(), './public/images'), 'utf-8')
+  /** 构建瀑布流组件需要的数据 */
   const waterfallItems = images.map(
     (image, index) => ({
       src: `/images/${image}`,
@@ -17,12 +16,12 @@ export default function Home() {
     })
   )
   return (
-    <div>
+    <>
       <Waterfall 
         items={waterfallItems}
         itemWidth={300}
         columns={3}
       />
-    </div>
+    </>
   )
 }
